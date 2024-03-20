db6327dcff33e323b4eacfad54e6bf76d6c3577e
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2024-03-20 20:30:40 +0800

    Update app-settings.ts

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
     public wsProviderEndpoint = 'wss://rococo-contracts-rpc.polkadot.io';
-    public apiEndpoint = 'https://dapp-api.gotem.io/';
+    public apiEndpoint = 'https://dapp-api.gotem.io';
     public keypair = localStorage.getItem("wallet-keypair") || "";
 }

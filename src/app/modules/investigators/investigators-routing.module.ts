073458b0f033262532c8d94cb13613e045e31fb1
karyllejaycaballero <118701394+karyllejaycaballero@users.noreply.github.com>
2024-05-07 14:25:13 +0800

    investigator module ui

--- src/app/modules/investigators/investigators-routing.module.ts
+++ src/app/modules/investigators/investigators-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvestigatorsComponent } from './investigators.component';
 
-const routes: Routes = [{ path: '', component: InvestigatorsComponent }];
+const routes: Routes = [
+  { path: '', component: InvestigatorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
